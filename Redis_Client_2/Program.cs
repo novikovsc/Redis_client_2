04fa8ba0834e7c5b9082a80234bb47b0d5495bce
nova1000 <65105567+nova1000@users.noreply.github.com>
2021-04-08 20:53:59 +0300

    add file

---
+++ Redis_Client_2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace Redis_Client_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = new ConfigurationOptions();
+            options.EndPoints.Add("localhost", 6379);
+
+            var redis = ConnectionMultiplexer.Connect(options);
+            var db = redis.GetDatabase(4);
+
+            Console.WriteLine("Чтобы продолжить нажмите любую клавишу, для выхода нажмите ESC");
+            var consoleKey = Console.ReadKey();
+            while (consoleKey.Key != ConsoleKey.Escape)
+            {
+                Console.Write("Введите 1 ключ: ");
+                var key1 = Console.ReadLine();
+                Console.Write("Введите 2 ключ: ");
+                var key2 = Console.ReadLine();
+
+                Console.WriteLine($"Результат {(int)db.StringGet(key1) * (int)db.StringGet(key2)}");
+
+                Console.WriteLine("Чтобы продолжить нажмите любую клавишу, для выхода нажмите ESC");
+                consoleKey = Console.ReadKey();
+            }
+        }
+    }
+}
